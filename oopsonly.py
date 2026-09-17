b30f19ffbdf4bde26d0fd005eea77b0c7a85921c
Aakash dubey <akashdubey6782@gmail.com>
2026-09-17 19:53:51 +0530

    questions solving in oops

--- oopsonly.py
+++ oopsonly.py
@@ -241,18 +241,18 @@ print(st.percentage)"""
         print(self.real,"i+",self.img,"j")
 
     #normal function
-    # def add(num1,num2):
-    #     newreal=num1.real+num2.real
-    #     newimg=num1.img+num2.img
-    #     num3=complex(newreal,newimg)
-    #     return num3
-
-    #dunder function
-    def __add__(num1,num2):
+    def add(num1,num2):
         newreal=num1.real+num2.real
         newimg=num1.img+num2.img
         num3=complex(newreal,newimg)
         return num3
+
+    #dunder function
+    # def __add__(num1,num2):
+    #     newreal=num1.real+num2.real
+    #     newimg=num1.img+num2.img
+    #     num3=complex(newreal,newimg)
+    #     return num3
 
 
 
@@ -262,5 +262,42 @@ num1.show()
 num2=complex(3,4)
 num2.show()
 
-num3=num1+num2
-num3.show()"""                       
+num3=num2.add(num1)  # for my funtion
+num3=num1+num2      #for dunder function because python automatically call it
+num3.show()                
+"""
+#questions
+"""class circle:
+    def __init__(self,radius):
+        self.radius=radius
+
+    def area(self):
+        return (3.14*self.radius*self.radius)
+
+    def perimeter(self):
+        return (2*3.14*self.radius)
+
+c1=circle(21)
+print(c1.area())
+print(c1.perimeter())"""
+
+"""class employee:
+    def __init__(self,role,department,salary):
+        self.role=role
+        self.department=department
+        self.salary=salary
+
+    def showdetails(self):
+        print(self.role,self.department,self.salary)
+
+class engineer(employee):
+    def __init__(self,name,age,role,salary,department):
+        self.name=name
+        self.age=age
+        super().__init__(role,department,salary)
+
+e1=engineer("Aakash",19,"manager",180000,"product")
+e1.showdetails()"""
+
+
+
